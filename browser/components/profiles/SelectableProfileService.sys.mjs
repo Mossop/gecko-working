f85e6be7845e21352225bfd19f7b32ecd5c45012
Dave Townsend <dtownsend@oxymoronical.com>
2024-10-18 15:37:35 +0100

    Bug 1925513: Recover `StoreID` from prefs in the event of a profiles.ini reset

--- browser/components/profiles/SelectableProfileService.sys.mjs
+++ browser/components/profiles/SelectableProfileService.sys.mjs
@@ -40,6 +40,7 @@ class SelectableProfileServiceClass {
   #asyncShutdownBlocker = null;
   #initialized = false;
   #groupToolkitProfile = null;
+  #storeID = null;
   #currentProfile = null;
   #everyWindowCallbackId = "SelectableProfileService";
 
@@ -53,6 +54,21 @@ class SelectableProfileServiceClass {
       this.#groupToolkitProfile =
         lazy.ProfileService.currentProfile ?? lazy.ProfileService.groupProfile;
     }
+
+    this.#storeID = this.#groupToolkitProfile?.storeID;
+
+    if (!this.#storeID) {
+      this.#storeID = Services.prefs.getCharPref(
+        "toolkit.profiles.storeID",
+        ""
+      );
+      if (this.#storeID) {
+        // This can happen if profiles.ini has been reset by a version of Firefox prior to 67 and
+        // the current profile is not the current default for the group. We can recover by
+        // attempting to find the group profile from the database.
+        this.restoreStoreID().catch(console.error);
+      }
+    }
   }
 
   // Do not use. Only for testing.
@@ -60,6 +76,10 @@ class SelectableProfileServiceClass {
     if (Cu.isInAutomation) {
       this.#groupToolkitProfile = mockToolkitProfile;
     }
+  }
+
+  get storeID() {
+    return this.#storeID;
   }
 
   get groupToolkitProfile() {
@@ -86,7 +106,7 @@ class SelectableProfileServiceClass {
   }
 
   async maybeCreateProfilesStorePath() {
-    if (this.#groupToolkitProfile.storeID) {
+    if (this.storeID) {
       return;
     }
 
@@ -100,6 +120,7 @@ class SelectableProfileServiceClass {
       .replace("{", "")
       .split("-")[0];
     this.#groupToolkitProfile.storeID = storageID;
+    this.#storeID = storageID;
     lazy.ProfileService.flush();
   }
 
@@ -108,7 +129,7 @@ class SelectableProfileServiceClass {
 
     return PathUtils.join(
       SelectableProfileServiceClass.PROFILE_GROUPS_DIR,
-      `${this.#groupToolkitProfile.storeID}.sqlite`
+      `${this.storeID}.sqlite`
     );
   }
 
@@ -123,7 +144,7 @@ class SelectableProfileServiceClass {
 
     // If the storeID doesn't exist, we don't want to create the db until we
     // need to so we early return.
-    if (!this.groupToolkitProfile.storeID) {
+    if (!this.storeID) {
       return;
     }
 
@@ -237,6 +258,20 @@ class SelectableProfileServiceClass {
     }
   }
 
+  async restoreStoreID() {
+    await this.init();
+
+    for (let profile of await this.getAllProfiles()) {
+      let groupProfile = lazy.ProfileService.getProfileByDir(profile.path);
+      if (groupProfile) {
+        this.#groupToolkitProfile = groupProfile;
+        this.#groupToolkitProfile.storeID = this.storeID;
+        lazy.ProfileService.flush();
+        return;
+      }
+    }
+  }
+
   async handleEvent(event) {
     switch (event.type) {
       case "activate": {
@@ -312,11 +347,12 @@ class SelectableProfileServiceClass {
    * and vacuum the group DB.
    */
   async deleteProfileGroup() {
-    if (this.getAllProfiles().length) {
+    if ((await this.getAllProfiles()).length) {
       return;
     }
 
     this.#groupToolkitProfile.storeID = null;
+    this.#storeID = null;
     lazy.ProfileService.flush();
     await this.vacuumAndCloseGroupDB();
   }
